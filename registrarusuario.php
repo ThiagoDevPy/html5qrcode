5a8406eb4079714601045b64f88422d9f626d18c
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-07 11:45:52 -0300

    validar si existe el alumno

--- registrarusuario.php
+++ registrarusuario.php
@@ -8,17 +8,34 @@ $cedula = $_POST['cedula'];
 $telefono = $_POST['telefono'];
 $carrera = $_POST['carrera'];
 
+$stmt = $conexion->prepare("SELECT * FROM empleados WHERE documento_numero = ?");
+$stmt->bind_param("s", $cedula);
+$stmt->execute();
+$result = $stmt->get_result();
 
 
-$sql = "INSERT INTO empleados (nombre,apellidos,documento_numero,telefono,carrera) VALUES (?,?,?,?,?)";
-$stmt = $conexion->prepare($sql);
-$stmt->bind_param('sssss', $nombre, $apellido, $cedula, $telefono,$carrera);
 
-if ($stmt->execute()) {
-    echo "Usuario registrado correctamente.";
-}else{
-    echo "Error al insertar" . $stmt->error;
-}
+    $stmt = $conexion->prepare("SELECT * FROM empleados WHERE documento_numero = ?");
+    $stmt->bind_param("s", $cedula );
+    $stmt->execute();
+    $resulta = $stmt->get_result();
+
+    if ($resulta->num_rows >= 1) {
+      echo  "Ya estas registrado";
+    } elseif ($resulta->num_rows == 0) {
+        // Aquí va la lógica para guardar los datos
+
+        $sql = "INSERT INTO empleados (nombre,apellidos,documento_numero,telefono,carrera) VALUES (?,?,?,?,?)";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bind_param('sssss', $nombre, $apellido, $cedula, $telefono,$carrera);
+        
+        if ($stmt->execute()) {
+            echo "Usuario registrado correctamente.";
+        }else{
+            echo "Error al insertar" . $stmt->error;
+        }
+    }
+
 
 
 ?>
