a2a26b6c9ab734a0789143dd0a6ad39a5353cfab
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-14 20:22:23 -0300

    new connetion

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'fra1.clusters.zeabur.com'; // Ejemplo: 'mysql-123456.c.database.azure.com'
-$port = '30089'; // Asegúrate de usar el puerto correcto
+$host = 'junction.proxy.rlwy'; // Ejemplo: 'mysql-123456.c.database.azure.com'
+$port = '57690'; // Asegúrate de usar el puerto correcto
 $user = 'root'; // Tu nombre de usuario
-$pass = 'F1452gwrd7t6MYJXEBAZz0pu98vWN3yj'; // Tu contraseña
-$dbname = 'asistencia'; // El nombre de la base de datos
+$pass = 'jTegmeDUEAScouxevhaXXazZTThvNptv'; // Tu contraseña
+$dbname = 'railway'; // El nombre de la base de datos
 
 // Crear la conexión
 $conexion = new mysqli($host, $user, $pass, $dbname, $port);
