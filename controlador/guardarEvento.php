2e6ad4363e7a90cb2fd029bfaa433fd9723509b0
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-15 15:24:54 -0300

    sadasdas

--- controlador/guardarEvento.php
+++ controlador/guardarEvento.php
@@ -1,7 +1,7 @@
 
 <?php
 ob_start(); // I
-session_start(); // Inicia la sesión
+
 
 if (isset($_POST['id'])) {
     $_SESSION['evento_id'] = $_POST['id']; // Guarda el ID en la sesión
--- guardarEvento.php
+++ guardarEvento.php
@@ -1,5 +1,4 @@
 <?php
-session_start(); // Inicia la sesión
 
 if (isset($_POST['id'])) {
     $_SESSION['evento_id'] = $_POST['id']; // Guarda el ID en la sesión
