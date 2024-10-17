b4b5498569a22143ca82f0d7b77f311d30d5db61
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-17 17:37:29 -0300

    new connectgion

--- config/conexion.php
+++ config/conexion.php
@@ -1,10 +1,10 @@
 <?php
-$host = 'junction.proxy.rlwy.net'; // Ejemplo: 'mysql-123456.c.database.azure.com'
-$port = '57690'; // Asegúrate de usar el puerto correcto
+$host = 'autorack.proxy.rlwy.net'; // Ejemplo: 'mysql-123456.c.database.azure.com'
+$port = '31433'; // Asegúrate de usar el puerto correcto
 $user = 'root'; // Tu nombre de usuario
-$pass = 'jTegmeDUEAScouxevhaXXazZTThvNptv'; // Tu contraseña
+$pass = 'SttfjyiqKhWHXUzGiGbokHowQCWFWpOQ'; // Tu contraseña
 $dbname = 'railway'; // El nombre de la base de datos
-
+mysql://root:SttfjyiqKhWHXUzGiGbokHowQCWFWpOQ@autorack.proxy.rlwy.net:31433/railway
 // Crear la conexión
 $conexion = new mysqli($host, $user, $pass, $dbname, $port);
 
