6b7c97b8cc5dbb60c71d46507cbeaa750c501fa4
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-19 09:14:53 -0300

    nre connetion

--- config/conexion.php
+++ config/conexion.php
@@ -1,15 +1,21 @@
 <?php
-$host = 'fra1.clusters.zeabur.com'; // Ejemplo: 'mysql-123456.c.database.azure.com'
-$port = '30467'; // Asegúrate de usar el puerto correcto
-$user = 'root'; // Tu nombre de usuario
-$pass = '3Hax6NeFyIRVC8Z712S5Pkmbg9Mcu40i'; // Tu contraseña
-$dbname = 'zeabur'; // El nombre de la base de datos
+include "database.php";
+// Configuración de la base de datos
+$host = 'fra1.clusters.zeabur.com';
+$port = '30467';
+$user = 'root';
+$pass = '3Hax6NeFyIRVC8Z712S5Pkmbg9Mcu40i';
+$dbname = 'zeabur';
 
-// Crear la conexión
-$conexion = new mysqli($host, $user, $pass, $dbname, $port);
+try {
+    // Crear una instancia de la clase Database
+    $db = new Database($host, $port, $user, $pass, $dbname);
+    $conexion = $db->getConnection();
 
-// Verificar la conexión
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    // Realiza operaciones en la base de datos aquí
+
+} catch (Exception $e) {
+    echo $e->getMessage(); // Muestra un mensaje genérico al usuario
 }
+?>
 
