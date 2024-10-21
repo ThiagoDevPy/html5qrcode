f01472bc0e66d5593b0262cd08b4f531bff98ebc
ThiagoDevPy <sebastiansaldivar092@gmail.com>
2024-10-21 11:02:42 -0300

    new connetion

--- config/conexion.php
+++ config/conexion.php
@@ -2,17 +2,15 @@
 include "database.php";
 // Configuración de la base de datos
 $host = 'fra1.clusters.zeabur.com';
-$port = '32505';
+$port = '31753';
 $user = 'root';
-$pass = '1L0Vy8R52pu3hsjCHwF7xqYB6gd9c4Dt';
+$pass = '8Plwn04fh936bKBTzi7Gu1rQSD5YMs2t';
 $dbname = 'zeabur';
 
 try {
     // Crear una instancia de la clase Database
     $db = new Database($host, $port, $user, $pass, $dbname);
     $conexion = $db->getConnection();
-
-    // Realiza operaciones en la base de datos aquí
 
 } catch (Exception $e) {
     echo $e->getMessage(); // Muestra un mensaje genérico al usuario
